955beb377930042604f7b7a949e45ff4fcfb1933
Charlie Azmitia <ccazmitia@gmail.com>
2019-07-18 07:55:10 -0400

    token

--- server.js
+++ server.js
@@ -2,21 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const yelp = require('yelp-fusion');
-const token = require('./token');
+// const token = require('./token');
+
+const token = process.env.TOKEN;
 const client = yelp.client(token);
 
 const port = process.env.PORT || 3000;
 
-// const allowCrossDomain = function(req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type');
-//   next();
-// };
-
 app.use(express.json());
-
-// app.use(allowCrossDomain);
 
 app.get('/app.js', (req, res, next) =>
   res.sendFile(path.join(__dirname, 'dist', 'main.js'))
